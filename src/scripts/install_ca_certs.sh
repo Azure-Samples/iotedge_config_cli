c962c80912944037bf2b93bb2aebb3042e3977eb
Lee <lefitche@microsoft.com>
2021-03-25 22:59:27 -0700

    fix install scripts

--- src/scripts/install_ca_certs.sh
+++ src/scripts/install_ca_certs.sh
@@ -5,5 +5,6 @@ update-ca-certificates
 # ======================= Copy device certs  =======================================
 cert_dir="/etc/aziot/certificates"
 mkdir -p $cert_dir
-cp "$device_id.hub-auth.cert.pem" "$cert_dir/$device_id.hub-auth.cert.pem"
-cp "$device_id.hub-auth.key.pem" "$cert_dir/$device_id.hub-auth.key.pem"
+cp "iotedge_config_cli_root.pem" "$cert_dir/iotedge_config_cli_root.pem"
+cp "$device_id.full-chain.cert.pem" "$cert_dir/$device_id.full-chain.cert.pem"
+cp "$device_id.key.pem" "$cert_dir/$device_id.key.pem"
