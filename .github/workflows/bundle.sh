ff10ffd5796c9493c86f5278974bf2c70fb260f8
Lee Fitchett <lefitche@microsoft.com>
2021-03-30 11:48:48 -0700

    Bundle templates in release (#5)
    * Bundle templates in release
    
    * fix release.yaml
    
    * make script executable
    
    * rename folder
    
    Co-authored-by: Varun Puranik <vpuranik@microsoft.com>

---
+++ .github/workflows/bundle.sh
@@ -0,0 +1,5 @@
+mkdir -p iotedge_config_cli_release
+cp target/release/iotedge_config_cli iotedge_config_cli_release/iotedge_config_cli
+cp -r templates/ iotedge_config_cli_release/templates
+
+tar -czvf iotedge_config_cli.tar.gz iotedge_config_cli_release
