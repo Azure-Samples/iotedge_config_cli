8a7c8aa474c5cb22eb1cf5e487c6257258ce7eea
Lee <lefitche@microsoft.com>
2021-03-30 11:47:27 -0700

    rename folder

--- .github/workflows/bundle.sh
+++ .github/workflows/bundle.sh
@@ -1,5 +1,5 @@
-mkdir -p temp_release
-cp target/release/iotedge_config_cli temp_release/iotedge_config_cli
-cp -r templates/ temp_release/templates
+mkdir -p iotedge_config_cli_release
+cp target/release/iotedge_config_cli iotedge_config_cli_release/iotedge_config_cli
+cp -r templates/ iotedge_config_cli_release/templates
 
-tar -czvf iotedge_config_cli.tar.gz temp_release
+tar -czvf iotedge_config_cli.tar.gz iotedge_config_cli_release
