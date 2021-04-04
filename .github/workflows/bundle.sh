923f2ef5759d14430a1fd383ced4a8686d7fce21
Lee <lefitche@microsoft.com>
2021-04-04 15:31:10 -0700

    update release script to rename binary

--- .github/workflows/bundle.sh
+++ .github/workflows/bundle.sh
@@ -1,5 +1,5 @@
 mkdir -p iotedge_config_cli_release
-cp target/release/iotedge_config_cli iotedge_config_cli_release/iotedge_config_cli
+cp target/release/iotedge_config_cli iotedge_config_cli_release/iotedge_config
 cp -r templates/ iotedge_config_cli_release/templates
 
 tar -czvf iotedge_config_cli.tar.gz iotedge_config_cli_release
